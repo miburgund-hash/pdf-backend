38570199bb7e7863588783716ea1f0370bb136b8
miburgund-hash <mi.burgund@gmail.com>
2025-09-05 12:21:17 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,3 @@
-// /api/index.js — Minimaltest, NUR Text ausgeben
 export default function handler(req, res) {
   res.setHeader('Content-Type', 'text/plain; charset=utf-8');
   res.status(200).send('PDF-API läuft ✔');
